3eb7605017ef7a3a40d288b63406d81ee499ef5d
HyeonSik04 <kmhnsh04@gmail.com>
2023-12-18 14:08:06 +0900

    기능 추가

--- student_sort/main.c
+++ student_sort/main.c
@@ -1,17 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include <Windows.h>
 
 typedef struct student {
 	int number;
 	char name[10];
+	double grade;
 } STUDENT;
 
-void sort(STUDENT* p, int size);
+void sort_number(STUDENT * p, int size);
+void sort_name(STUDENT* p, int size);
+void sort_grade(STUDENT* p, int size);
 
 int main() {
-	int size;
+	int size, num;
 
 	printf("학생 수: ");
 	scanf("%d", &size);
@@ -31,10 +35,34 @@ int main() {
 		scanf("%d", &((p + i)->number));
 		printf("이름: ");
 		scanf("%s", &((p + i)->name));
+		printf("학점: ");
+		scanf("%lf", &((p + i)->grade));
 		printf("\n");
 	}
+	system("cls");
 
-	sort(p, size);
+	while (1) {
+		printf("정렬 기준: 1.학번		2. 이름		3. 학점\n");
+		scanf("%d", &num);
+		if (num == 1) {
+			sort_number(p, size);
+			break;
+		}
+		else if (num == 2) {
+			sort_name(p, size);
+			break;
+		}
+		else if (num == 3) {
+			sort_grade(p, size);
+			break;
+		}
+		else {
+			system("cls");
+			printf("다시 선택\n");
+			continue;
+		}
+	}
+
 
 	FILE* fp;
 	fp = fopen("student.txt", "w");
@@ -48,6 +76,7 @@ int main() {
 		fprintf(fp, "학생 #%d\n", i + 1);
 		fprintf(fp, "학번: %d\n", ((p + i)->number));
 		fprintf(fp, "이름: %s\n", ((p + i)->name));
+		fprintf(fp, "학점: %.2lf\n", ((p + i)->grade));
 		fprintf(fp, "\n");
 	}
 
@@ -57,18 +86,50 @@ int main() {
 	return 0;
 }
 
-void sort(STUDENT* p, int size) {
-	int least;
+void sort_number(STUDENT* p, int size) {
+	int min;
 	STUDENT temp;
 	for (int i = 0; i < size; i++) {
-		least = i;
+		min = i;
 		for (int j = i + 1; j < size; j++) {
-			if (((p + least)->number) > (p + j)->number) {
-				least = j;
+			if (((p + min)->number) > (p + j)->number) {
+				min = j;
 			}
 		}
 		temp = *(p + i);
-		*(p + i) = *(p + least);
-		*(p + least) = temp;
+		*(p + i) = *(p + min);
+		*(p + min) = temp;
+	}
+}
+
+void sort_name(STUDENT* p, int size) {
+	int min;
+	STUDENT temp;
+	for (int i = 0; i < size; i++) {
+		min = i;
+		for (int j = i + 1; j < size; j++) {
+			if (((p + min)->number) > (p + j)->number) {
+				min = j;
+			}
+		}
+		temp = *(p + i);
+		*(p + i) = *(p + min);
+		*(p + min) = temp;
+	}
+}
+
+void sort_grade(STUDENT* p, int size) {
+	int max;
+	STUDENT temp;
+	for (int i = 0; i < size; i++) {
+		max = i;
+		for (int j = i + 1; j < size; j++) {
+			if (((p + max)->grade) < (p + j)->grade) {
+				max = j;
+			}
+		}
+		temp = *(p + i);
+		*(p + i) = *(p + max);
+		*(p + max) = temp;
 	}
 }
